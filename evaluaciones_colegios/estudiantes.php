b4eaf44a67ee58aceb19009b5344a9cb7acb73e2
llancor <llancor@gmail.com>
2026-09-04 00:11:29 -0400

    Delete evaluaciones_colegios directory

--- evaluaciones_colegios/estudiantes.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-require_once __DIR__ . '/seguridad.php';
-simceRequireRole(array('colegio_admin'), false);
-header('Content-Type: text/html; charset=utf-8');
-header('Cache-Control: no-store');
-readfile(__DIR__ . '/estudiantes.html');
